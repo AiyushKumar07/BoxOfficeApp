9e56799ca57a789e6448de6857b9202c647ca9cf
Aiyush Kumar <aiyushkanojia@gmail.com>
2023-09-23 20:39:42 +0530

    Added Cards For Shows and Actors

---
+++ src/components/shows/ShowCard.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+const ShowCard = ({ name, image, id, summary }) => {
+  const summaryStripped = summary
+    ? summary.split(' ').slice(0, 10).join(' ').replace(/<.+?>/g, '')
+    : 'No description';
+  return (
+    <div>
+      <div>
+        <img src={image} alt="" />
+      </div>
+      <h1>{name}</h1>
+      <p>{summaryStripped}</p>
+      <div>
+        <Link to="/">Read More</Link>
+        <button type="button">Star Me</button>
+      </div>
+    </div>
+  );
+};
+
+export default ShowCard;
