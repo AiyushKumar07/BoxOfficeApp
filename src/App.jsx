68b2d2e353dbb78b1238903808031d97b318af02
Aiyush Kumar <aiyushkanojia@gmail.com>
2023-09-25 16:15:53 +0530

    Added Styles For Home Page using StyledComponents and Created Global Theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,30 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { GlobalTheme } from './theme';
 import Home from './pages/Home';
 import Starred from './pages/Starred';
 import MainLayout from './components/MainLayout';
 import Show from './pages/Show';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<MainLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/starred" element={<Starred />} />
-          </Route>
-          <Route path="/show/:showId" element={<Show />} />
+      <GlobalTheme>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<MainLayout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/starred" element={<Starred />} />
+            </Route>
 
-          <Route path="*" element={<>Not Found</>} />
+            <Route path="/show/:showId" element={<Show />} />
 
-          {/* <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="teams" element={<Teams />}>
-            <Route path=":teamId" element={<Team />} />
-            <Route path="new" element={<NewTeamForm />} />
-            <Route index element={<LeagueStandings />} />
-          </Route>
-        </Route>
-        <Route element={<PageLayout />}>
-          <Route path="/privacy" element={<Privacy />} />
-          <Route path="/tos" element={<Tos />} />
-        </Route>
-        <Route path="contact-us" element={<Contact />} /> */}
-        </Routes>
-      </BrowserRouter>
+            <Route path="*" element={<>Not Found</>} />
+          </Routes>
+        </BrowserRouter>
+      </GlobalTheme>
     </QueryClientProvider>
   );
 }
